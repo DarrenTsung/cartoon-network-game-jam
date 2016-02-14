aedfaca20a6c20044f34f4d00604230138f1ea9f
Darren Tsung <darren.tsung@gmail.com>
2016-02-13 22:57:33 -0800

    Spacing for buttons + beginning animations

--- cartoon-network-game-jam-unity/Assets/GameSpecific/Code/Battling/BattleSequenceManager.cs
+++ cartoon-network-game-jam-unity/Assets/GameSpecific/Code/Battling/BattleSequenceManager.cs
@@ -51,7 +51,7 @@ namespace DT.Game {
       foreach (string prefabName in opponentPrefabNames) {
         bool indexIsEven = index % 2 == 0;
         GameObject opponentObject = Toolbox.GetInstance<ObjectPoolManager>().Instantiate(prefabName);
-        opponentObject.transform.position = new Vector3(4.0f + ((heightIndex % 2 == 0) ? 1.0f : 0.0f), heightIndex * heightOffset, 0.0f);
+        opponentObject.transform.position = new Vector3(2.0f + (heightIndex * 1.5f), heightIndex * heightOffset, 0.0f);
 
         Actor actor = opponentObject.GetRequiredComponentInChildren<Actor>();
         Battle.Instance.badGuys.Add(actor);
